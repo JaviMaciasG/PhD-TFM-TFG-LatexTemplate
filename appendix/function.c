bd95ee0dea9f069df8c390a5f039c967368ed13b
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2013-11-25 11:21:32 +0000

    Playing with source code examples. Listing of codefloat still does not work.

---
+++ appendix/function.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+// Esto es una función de prueba
+void funcionPrueba(int argumento)
+{	
+	int prueba = 1;
+
+  printf("Esto es una prueba [%d][%d]\n", argumento, prueba);
+
+}
---
+++ appendix/hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+#define LOOP_TIMES 5
+
+int main(int argc, char* argv[]) 
+{
+  int i;
+
+  for (i = 1; i < LOOP_TIMES; i++)
+    puts("Hola mundo!");
+}
