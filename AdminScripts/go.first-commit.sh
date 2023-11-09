81506bdf8d40f5fbe0ee3954fb7fe2e73de628a1
Javier Macias-Guarasa <javier.maciasguarasa@uah.es>
2023-11-09 11:49:32 +0100

    First commit

---
+++ AdminScripts/go.first-commit.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+git init .
+git add README.md
+git add Config/*
+git add Anteproyecto/*tex Anteproyecto/Makefile
+
+git add Book/book.tex
+git add Book/Makefile
+
+git add Book/chapters/*tex
+git add Book/biblio/*bib Book/biblio/*tex
+
+git add Book/diagrams/Diagrama2.pdf
+
+git add Book/cover/*tex
+git add Book/abstract/*tex
+git add Book/acronyms/*tex
+git add Book/dedication/dedicatoria.tex
+git add Book/logos/*png Book/logos/*jpg Book/logos/*pdf
+git add Book/letters/sampleLetter.pdf
+git add Book/pliego/*tex
+git add Book/presupuesto/*tex
+git add Book/acknowledgements/agradecimientos.tex
+git add Book/additionalContributors.txt
+git add Book/symbols/*tex
+git add Book/appendix/*.tex
+git add Book/logos/uah/*pdf
+git add Papeleo/TFGAutorizacionPubAbierto/*tex Papeleo/TFGAutorizacionPubAbierto/Makefile
+git add Papeleo/TFMAutorizacionPubAbierto/*tex Papeleo/TFMAutorizacionPubAbierto/Makefile
+git add Papeleo/TFGVistoBuenoTutor/*tex Papeleo/TFGVistoBuenoTutor/Makefile
+git add Papeleo/TFMVistoBuenoTutor/*tex Papeleo/TFMVistoBuenoTutor/Makefile
